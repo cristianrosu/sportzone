275b7376f4f3b4f945f649e7682bc1236230aa40
Tudor Pavel <tudorv.pavel@gmail.com>
2015-02-21 15:10:43 +0200

    Set up API routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
+require 'api_version'
+
 Rails.application.routes.draw do
   resources :venues
 
   root to: 'visitors#index'
   devise_for :users
+
+  namespace :api, defaults: { format: 'json' } do
+    scope module: :v1, constraints: APIVersion.new('v1', true) do
+      resources :venues, only: [:index, :show]
+    end
+  end
 end
