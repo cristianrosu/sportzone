a9e3e435cf41040d8deeb0ea9733c05e28ac3bff
Tudor Pavel <tudorv.pavel@gmail.com>
2015-02-21 18:29:10 +0200

    Move chosen script in a separate file, keep application.js just for including other files.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,16 +20,3 @@
 //= require gmaps/google
 
 //= require_tree .
-
-
-
-var ready = function() {
-    $('.chosen-select').chosen({
-        no_results_text: 'No results matched'
-    });
-}
-
-
-$('document').ready(ready);
-/* Page load is what turbo links will trigger. */
-$(document).on('page:load', ready);
