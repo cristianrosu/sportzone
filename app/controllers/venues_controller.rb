55d30aa2b6387b5023936bf4c431624931fa7083
Tudor Pavel <tudorv.pavel@gmail.com>
2015-02-21 15:42:27 +0200

    Move JSON functionality inside proper API place and clean up after us in VenuesController.

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -2,13 +2,11 @@ class VenuesController < ApplicationController
   before_action :set_venue, only: [:show, :edit, :update, :destroy]
 
   # GET /venues
-  # GET /venues.json
   def index
     @venues = Venue.all
   end
 
   # GET /venues/1
-  # GET /venues/1.json
   def show
   end
 
@@ -22,43 +20,29 @@ class VenuesController < ApplicationController
   end
 
   # POST /venues
-  # POST /venues.json
   def create
     @venue = Venue.new(venue_params)
 
-    respond_to do |format|
-      if @venue.save
-        format.html { redirect_to @venue, notice: 'Venue was successfully created.' }
-        format.json { render :show, status: :created, location: @venue }
-      else
-        format.html { render :new }
-        format.json { render json: @venue.errors, status: :unprocessable_entity }
-      end
+    if @venue.save
+      redirect_to @venue, notice: 'Venue was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /venues/1
-  # PATCH/PUT /venues/1.json
   def update
-    respond_to do |format|
-      if @venue.update(venue_params)
-        format.html { redirect_to @venue, notice: 'Venue was successfully updated.' }
-        format.json { render :show, status: :ok, location: @venue }
-      else
-        format.html { render :edit }
-        format.json { render json: @venue.errors, status: :unprocessable_entity }
-      end
+    if @venue.update(venue_params)
+      redirect_to @venue, notice: 'Venue was successfully updated.'
+    else
+      render :edit
     end
   end
 
   # DELETE /venues/1
-  # DELETE /venues/1.json
   def destroy
     @venue.destroy
-    respond_to do |format|
-      format.html { redirect_to venues_url, notice: 'Venue was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to venues_url, notice: 'Venue was successfully destroyed.'
   end
 
   private
--- app/views/venues/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@venues) do |venue|
-  json.extract! venue, :id, :name, :description, :city, :address, :latitude, :longitude, :phone, :email, :url_website, :url_facebook, :other_info, :contributor_id, :status
-  json.url venue_url(venue, format: :json)
-end
